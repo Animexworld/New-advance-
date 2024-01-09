785ea9d4e5b73cc52e6bd1ced0f9b18e200ab50a
Animexworld <155986696+Animexworld@users.noreply.github.com>
2024-01-09 22:51:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
-cd boted;
-pip3 install -U -r requirements.txt;
-echo "Bot Started....";
-python3 bot.py;
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/@Animexworld0_bot/New-advance- /New-advance-
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /New-advance-
+fi
+cd /New-advance-
+pip3 install -U -r requirements.txt
+echo "Bot Started...."
+python3 bot.py
